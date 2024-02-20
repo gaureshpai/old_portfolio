1e962b371b6a17c769d2047068534c0a909f2260
Gauresh G Pai <paigauresh@gmail.com>
2024-02-20 18:46:35 +0530

    add icons

--- app/ContactInfo/page.tsx
+++ app/ContactInfo/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React from 'react';
 import { FaEnvelope, FaLinkedin, FaTwitter, FaInstagram, FaGithub, FaYoutube } from 'react-icons/fa';
 
--- app/Skills/page.tsx
+++ app/Skills/page.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import {
+  FaPython,
+  FaJava,
+  FaHtml5,
+  FaCss3Alt,
+  FaReact,
+  FaNodeJs,
+  FaBootstrap,
+  FaDatabase,
+  FaGit,
+  FaTools,
+  FaChartBar
+} from 'react-icons/fa';
+
 
 const Skills = () => {
   return (
@@ -7,34 +21,34 @@ const Skills = () => {
         <h2>Skills</h2>
         <h3>Programming Languages</h3>
         <ul>
-          <li>Python</li>
-          <li>Java</li>
-          <li>C++</li>
-          <li>C</li>
-          <li>Scratch</li>
+          <li><FaPython /> Python</li>
+          <li><FaJava /> Java</li>
+          <li> C++</li>
+          <li> C</li>
+          <li> Scratch</li>
         </ul>
 
         <h3>Web Development</h3>
         <ul>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>React.js</li>
-          <li>Node.js</li>
-          <li>Bootsrap</li>
+          <li><FaHtml5 /> HTML5</li>
+          <li><FaCss3Alt /> CSS3</li>
+          <li><FaReact /> React.js</li>
+          <li><FaNodeJs /> Node.js</li>
+          <li><FaBootstrap /> Bootstrap</li>
         </ul>
 
         <h3>Database</h3>
         <ul>
-          <li>SQL</li>
-          <li>MongoDB</li>
-          <li>Firebase</li>
+          <li><FaDatabase /> SQL</li>
+          <li><FaDatabase /> MongoDB</li>
+          <li><FaDatabase /> Firebase</li>
         </ul>
 
         <h3>Other Skills</h3>
         <ul>
-          <li>Version Control (Git)</li>
-          <li>Problem Solving</li>
-          <li>Marketing</li>
+          <li><FaGit /> Version Control (Git)</li>
+          <li><FaTools /> Problem Solving</li>
+          <li><FaChartBar /> Marketing</li>
         </ul>
       </div>
     </div>
