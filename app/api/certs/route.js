a6fa1d0e57893f319addf501e26ef8a39e60f836
Gauresh G Pai <paigauresh@gmail.com>
2024-02-22 05:22:20 +0530

    impoved import

--- app/api/certs/route.js
+++ app/api/certs/route.js
@@ -1,14 +1,15 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next';
+
 import { connect } from '../../../utils/db';
-import Cert from '../../models/Cert'
+import Cert from '../../models/Cert';
 
 export const GET = async (request) => {
     try {
-        await connect(); 
+        await connect();
 
         const posts = await Cert.find();
 
-        return new NextResponse(JSON.stringify(posts),{status:200});
+        return new NextResponse(JSON.stringify(posts), { status: 200 });
     } catch (error) {
         console.error("Error in connecting to the database:", error);
         return new NextResponse("Error in connecting to the database", { status: 500 });
